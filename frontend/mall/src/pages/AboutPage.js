9dafa3e43764e7dd8fb4a948c38eb101f19faafe
hamjinoo <166359273+hamjinoo@users.noreply.github.com>
2024-04-21 00:49:14 +0900

    프론트엔드 초기 세팅

---
+++ frontend/mall/src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BasicLayout from '../layouts/BasicLayout';
+
+function AboutPage() {
+    return (
+        <BasicLayout>
+            <div>About Page</div>
+        </BasicLayout>
+    );
+}
+
+export default AboutPage;
---
+++ frontend/mall/src/pages/todo/ReadPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function ReadPage() {
+
+    const { obj } = useParams()
+
+
+
+    return <div>ReadPage</div>;
+}
+
+export default ReadPage;
